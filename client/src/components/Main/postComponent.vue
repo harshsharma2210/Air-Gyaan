fe0d135a19be5b277a19ae4b8e6c6f4051b88e52
Sravan <sravan.banty@gmail.com>
2020-09-26 00:53:40 +0530

    post component done.

---
+++ client/src/components/Main/postComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card class="mx-auto" color="primary" dark max-width="1000">
+    <v-card-title>
+      <v-icon large left>mdi-twitter</v-icon>
+      <span class="title font-weight-light text-capitalize" color="secondary">{{content.catagory}}</span>
+    </v-card-title>
+
+    <v-card-text
+      class="headline font-weight-bold"
+    >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."{{content.name}}</v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-light text-capitalize">{{content.createdBy.name}}</v-list-item-title>
+        </v-list-item-content>
+
+        <v-row align="center" justify="end">
+          <v-icon class="mr-1">mdi-heart</v-icon>
+          <span class="subheading mr-2">{{content.info.like}}</span>
+          <span class="mr-1">·</span>
+          <v-icon class="mr-1">mdi-share-variant</v-icon>
+          <span class="subheading">45</span>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "post",
+  props: {
+    content: {
+      type: Object,
+    },
+  },
+  data: () => ({}),
+};
+</script>
---
+++ client/src/components/post.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container fluid>
+    <v-row v-for="(post,index) in posts" :key="index">
+      <postComponent class="ma-3 pa-0" :content="post"></postComponent>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import postComponent from "@/components/Main/postComponent";
+export default {
+  name: "HelloWorld",
+  components: {
+    postComponent,
+  },
+  data: () => ({
+    posts: [
+      {
+        name: "content-1",
+        text: "content",
+        catagory: "catagory",
+        createdBy: {
+          name: "name of creator",
+          profile: "link for profile picture",
+        },
+        info: { like: 23, share: 345 },
+      },
+      {
+        name: "content-2",
+        text: "content",
+        catagory: "catagory",
+        createdBy: {
+          name: "name of creator",
+          profile: "link for profile picture",
+        },
+        info: { like: 234, share: 345 },
+      },
+      {
+        name: "content-3",
+        text: "content",
+        catagory: "catagory",
+        createdBy: {
+          name: "name of creator",
+          profile: "link for profile picture",
+        },
+        info: { like: 2389, share: 345 },
+      },
+      {
+        name: "content-4",
+        text: "content",
+        catagory: "catagory",
+        createdBy: {
+          name: "name of creator",
+          profile: "link for profile picture",
+        },
+        info: { like: 234, share: 345 },
+      },
+    ],
+  }),
+};
+</script>
