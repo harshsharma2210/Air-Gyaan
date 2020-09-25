9f7634adb75c13c5cd99be38b38f7983e108705a
Sravan <sravan.banty@gmail.com>
2020-09-26 00:54:01 +0530

    Profile component done

---
+++ client/src/components/User/profile.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container fluid>
+        <v-layout column>
+            <v-card>
+                <v-card-text>
+                    <v-flex class="mb-4">
+                        <v-avatar size="96" class="mr-4">
+                            <img src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light" alt="Avatar">
+                        </v-avatar>
+                        <v-btn @click="openAvatarPicker">Change Avatar</v-btn>
+                    </v-flex>
+                    <v-text-field
+                        v-model="form.firstName"
+                        label="FirstName"></v-text-field>
+                    <v-text-field
+                        v-model="form.lastName"
+                        label="Last Name"></v-text-field>
+                    <v-text-field
+                        v-model="form.contactEmail"
+                        label="Email Address"></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" :loading="loading" @click.native="update">
+                        <v-icon left dark>mdi-check</v-icon>
+                        Save Changes
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        pageTitle: 'My Profile',
+        data () {
+            return {
+                loading: false,
+                form: {
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    contactEmail: 'john@doe.com',
+                    avatar: 'MALE_CAUCASIAN_BLOND_BEARD'
+                },
+                showAvatarPicker: false
+            }
+        },
+        methods: {
+            openAvatarPicker () {
+                this.showAvatarPicker = true
+            },
+            selectAvatar (avatar) {
+                this.form.avatar = avatar
+            }
+        }
+    }
+</script>
