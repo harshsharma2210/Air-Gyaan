d1664b1a1fdbc317017233051ffe5d09e634d3cf
theacehorsemen <65445074+theacehorsemen@users.noreply.github.com>
2020-09-27 20:06:06 +0530

    Completed The Post Box
    Completed the Post Box on route ./new_post

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+import 'tiptap-vuetify/dist/main.css'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
 
@@ -15,3 +18,17 @@ export default new Vuetify({
       },
     },
   });
+
+
+
+const vuetify = new Vuetify()
+ 
+// use Vuetify's plugin
+Vue.use(Vuetify)
+// use this package's plugin
+Vue.use(TiptapVuetifyPlugin, {
+  // the next line is important! You need to provide the Vuetify Object to this place.
+  vuetify, // same as "vuetify: vuetify"
+  // optional, default to 'md' (default vuetify icons before v2.0.0)
+  iconsGroup: 'md'
+})
