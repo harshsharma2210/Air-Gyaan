f02dc41c56db121e56e5669e95996f7f2614f13e
theacehorsemen <65445074+theacehorsemen@users.noreply.github.com>
2020-09-27 21:46:41 +0530

    Final post box
    Finally completed post box with padding and rounded corners

--- client/src/components/new_post.vue
+++ client/src/components/new_post.vue
@@ -1,12 +1,59 @@
 <template>
-  <div>
-    <h2 class="mb-3">
-      New Post!
-    </h2>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          create Post
+        </v-btn>
 
-    <tiptap-vuetify v-model="content" :extensions="extensions" placeholder="Write something …"/>
-  </div>
+      </template>
+      <v-card class="pa-md-7 mx-lg-auto" id="rounded-card">
+        <template>
+          <div>
+            <h2 class="mb-3">
+              New Post!
+            </h2>
+
+            <tiptap-vuetify v-model="content" :extensions="extensions" placeholder="Write something …"/>
+          </div>
+        </template>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Post!
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
+
+<style lang="css">
+#rounded-card{
+    border-radius: 30px;
+}
+</style>
 
 <script>
 import {
@@ -45,7 +92,9 @@ export default {
       Paragraph,
       HardBreak
     ],
-    content: ''
+    content: '',
+    dialog: false,
+    max: 250
   })
 }
 </script>
--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -23,12 +23,10 @@ export default new Vuetify({
 
 const vuetify = new Vuetify()
  
-// use Vuetify's plugin
+
 Vue.use(Vuetify)
-// use this package's plugin
+
 Vue.use(TiptapVuetifyPlugin, {
-  // the next line is important! You need to provide the Vuetify Object to this place.
-  vuetify, // same as "vuetify: vuetify"
-  // optional, default to 'md' (default vuetify icons before v2.0.0)
+  vuetify,
   iconsGroup: 'md'
 })
