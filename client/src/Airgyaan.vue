42e843b0c1578755d049cad7a7d6ff23bba0ed10
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2020-10-17 22:37:05 +0200

    rename app, include check-context to survey signed-in on f5

---
+++ client/src/Airgyaan.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app :dark="$vuetify.theme.dark">
+    <v-theme-provider root>
+      <default-layout />
+    </v-theme-provider>
+  </v-app>
+</template>
+
+<script>
+import { useDarkMode } from "@/utils/useDarkMode";
+import apiFetch from "@/mixins/apiFetch";
+
+export default {
+  name: 'Airgyaan',
+  mixins: [apiFetch],
+  components: {
+    DefaultLayout: () => import(/* webpackChunkName: "layout-dependencies" */ "@/components/DefaultLayout")
+  },
+  data: () => ({
+    darkDetector: null,
+    nativeDarkMode: undefined
+  }),
+  async beforeMount() {
+    await this.$nextTick();
+    // eslint-disable-next-line no-unused-vars
+    this.darkDetector = useDarkMode(
+        this.$vuetify,
+        dark => (this.nativeDarkMode = dark)
+    )
+    await this.checkContext();
+  },
+  async beforeDestroy() {
+    this.darkDetector && this.darkDetector.destroy();
+    this.darkDetector = null;
+  }
+};
+</script>
