5d4ea2068779beca9d08de1fdf059c38d02760d1
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2020-11-15 14:38:34 +0100

    seo: adjust title and description on page transition + locale aware => change title + description on locale change event

--- client/src/AirGyaan.vue
+++ client/src/AirGyaan.vue
@@ -2,6 +2,7 @@
   <v-app :dark="$vuetify.theme.dark">
     <v-theme-provider root>
       <default-layout />
+      <default-layout />
     </v-theme-provider>
   </v-app>
 </template>
@@ -12,6 +13,8 @@ const mockUiLogin = process.env.VUE_APP_MOCK_UI_LOGIN === "true";
 
 import { useDarkMode } from "@/utils/useDarkMode";
 import apiFetch from "@/mixins/apiFetch";
+import camelCase from "lodash/camelCase";
+import upperFirst from "lodash/upperFirst";
 
 export default {
   name: 'AirGyaan',
@@ -23,6 +26,14 @@ export default {
     darkDetector: null,
     nativeDarkMode: undefined
   }),
+  watch: {
+    "$i18n.locale": {
+      async handler() {
+        await this.changeSeo(this.$route);
+      },
+      immediate: true
+    }
+  },
   async beforeMount() {
     await this.$nextTick();
     // eslint-disable-next-line no-unused-vars
@@ -30,6 +41,7 @@ export default {
         this.$vuetify,
         dark => (this.nativeDarkMode = dark)
     )
+    this.$router.afterEach(this.changeSeo);
     await this.checkContext();
   },
   async beforeDestroy() {
@@ -37,6 +49,23 @@ export default {
     this.darkDetector = null;
   },
   methods: {
+    async changeSeo(to) {
+      if (to) {
+        const { meta } = to;
+        let title = meta && meta.title;
+        if (!title || title.length === 0) {
+          title = this.$t(`Views.${upperFirst(camelCase(to.name))}.title`);
+        }
+        document.title = [
+          this.$t(title),
+          this.$t("App.title")
+        ].join(" - ");
+        const description = meta && meta.description;
+        if (description) {
+          document.querySelector('meta[name="description"]').setAttribute("content", description);
+        }
+      }
+    },
     async checkContext() {
       if (mockUiLogin === false) {
         await apiFetch.methods.checkContext.call(this);
