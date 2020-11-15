d53d895b2935be3a3b89b7ae203650de295b5ff4
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2020-11-15 14:41:30 +0100

    inject sign-in function adjust code to call signIn function instead emit event validation is now lazy added buttons to sign in with social media: pending logic

--- client/src/components/SignIn.vue
+++ client/src/components/SignIn.vue
@@ -1,83 +1,95 @@
 <template>
-  <v-card>
-    <v-card-title>{{ title }}</v-card-title>
-    <v-card-text>
-      <v-form class="signin-form pt-4" @submit.prevent="fireSubmit">
-        <v-text-field
-            ref="username"
-            v-model="form.username"
-            :label="$t('Components.User.signin.username')"
-            :rules="usernameRules"
-            counter
-            dense
-            @keyup.enter.prevent="fireSubmit"
-        ></v-text-field>
-        <!-- passwords MUST have an id -->
-        <v-text-field
-            id="password"
-            ref="password"
-            :type="passwordType"
-            v-model="form.password"
-            :append-icon="passwordIcon"
-            :label="$t('Components.User.signin.password')"
-            :rules="passwordRules"
-            counter
-            dense
-            autocomplete="off"
-            @click:append="showPassword = !showPassword"
-            @keyup.enter.prevent="fireSubmit"
-        ></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-          ref="access"
-          outlined
-          class="ma-2 px-4"
-          :loading="busy"
-          @click.native.prevent="fireSubmit"
-      >
-        {{ $t("Components.User.signin.google") }}
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-          ref="access"
-          outlined
-          class="ma-2 px-4"
-          :loading="busy"
-          @click.native.prevent="fireSubmit"
-      >
-        {{ $t("Components.User.signin.facebook") }}
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-          ref="access"
-          outlined
-          class="ma-2 px-4"
-          :loading="busy"
-          @click.native.prevent="fireSubmit"
-      >
-        {{ $t("Components.User.signin.linkedin") }}
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-          ref="access"
-          outlined
-          class="ma-2 px-4"
-          :loading="busy"
-          @click.native.prevent="fireSubmit"
-      >
-        {{ $t("Components.User.signin.signin") }}
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div :class="['sign-in-container', { mobile: $vuetify.breakpoint.xsOnly }]">
+    <v-card class="elevation-0 border-0">
+      <v-card-title class="justify-center">{{ title }}</v-card-title>
+      <v-card-text>
+        <v-form ref="form" class="signin-form pt-4" @submit.prevent="fireSubmit"  lazy-validation>
+          <v-text-field
+              ref="username"
+              v-model="form.username"
+              :label="$t('Components.User.signin.username')"
+              :rules="usernameRules"
+              counter
+              dense
+              @keyup.enter.prevent="fireSubmit"
+          ></v-text-field>
+          <!-- passwords MUST have an id -->
+          <v-text-field
+              id="password"
+              ref="password"
+              :type="passwordType"
+              v-model="form.password"
+              :append-icon="passwordIcon"
+              :label="$t('Components.User.signin.password')"
+              :rules="passwordRules"
+              counter
+              dense
+              autocomplete="off"
+              @click:append="showPassword = !showPassword"
+              @keyup.enter.prevent="fireSubmit"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            ref="access"
+            outlined
+            class="ma-2 px-4"
+            :loading="busy"
+            @click.native.prevent="fireSubmit"
+        >
+          {{ $t("Components.User.signin.signin") }}
+        </v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        <v-btn
+            ref="google"
+            outlined
+            small
+            class="ma-2 px-4"
+            :loading="busy"
+            @click.native.prevent="fireSubmit"
+        >
+          <v-icon left>$vuetify.icons.google</v-icon>
+          {{ $t("Components.User.signin.google") }}
+        </v-btn>
+        <v-btn
+            ref="facebook"
+            outlined
+            small
+            class="ma-2 px-4"
+            :loading="busy"
+            @click.native.prevent="fireSubmit"
+        >
+          <v-icon left>$vuetify.icons.facebook</v-icon>
+          {{ $t("Components.User.signin.facebook") }}
+        </v-btn>
+        <v-btn
+            ref="linkedin"
+            outlined
+            small
+            class="ma-2 px-4"
+            :loading="busy"
+            @click.native.prevent="fireSubmit"
+        >
+          <v-icon left>$vuetify.icons.linkedin</v-icon>
+          {{ $t("Components.User.signin.linkedin") }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 <script>
 import { mapState } from "vuex";
 
 export default {
   name: "sign-in",
+  inject: {
+    signIn: {
+      default: null
+    }
+  },
   data: () => ({
     form: {
       username: null,
@@ -88,7 +100,7 @@ export default {
   computed: {
     ...mapState(["busy"]),
     title() {
-      return this.$t("Components.User.signin.title", [this.$t("App.title")]);
+      return this.$t("Components.User.signin.card-title", [this.$t("App.title")]);
     },
     usernameRules() {
       return this.fieldValidator();
@@ -127,7 +139,17 @@ export default {
       this.form.password = null;
     },
     async fireSubmit() {
-      this.$emit("signin", this.formValues);
+      let valid = false;
+      try {
+        if (this.$refs.form) {
+          valid = await this.$refs.form.validate();
+        }
+      } catch (e) {
+        console.error("cannot validate form", e);
+      }
+      if (valid) {
+        await this.signIn(this.formValues);
+      }
     },
     async focusElement(initial = false, el = "username") {
       if (initial) {
@@ -152,10 +174,26 @@ export default {
 }
 </script>
 <style lang="scss">
-.v-form.signin-form {
+.sign-in-container {
   display: grid;
-  grid-template-columns: 1fr;
-  grid-auto-rows: min-content;
-  grid-row-gap: 8px;
+  min-width: 100%;
+  .v-form.signin-form {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-auto-rows: min-content;
+    grid-row-gap: 8px;
+  }
+  .v-card__actions + .v-card__actions {
+    display: grid;
+    grid-template-rows: min-content;
+    grid-template-columns: 1fr;
+  }
+  &:not(.mobile) {
+    place-items: center;
+    min-width: unset !important;
+    .v-form.signin-form {
+      grid-template-columns: minmax(385px, 25%) !important;
+    }
+  }
 }
 </style>
